347a7c9fa5a36223c597b68b7d34cac191a92966
michael-codelibrary <michael@fame.me>
2026-07-29 17:57:31 +0800

    Levels: shift col-2 background -120px left

--- components/Levels.tsx
+++ components/Levels.tsx
@@ -51,7 +51,7 @@ function StarIcon() {
 
 const levels = [
   { name: "FIRST TOUCH",     desc: "Never played, or not for years",      color: "#484848", Icon: TouchIcon,  bg: "/col-1.png", bgPos: "left center" },
-  { name: "DEVELOPMENT",     desc: "Plays a bit, wants to get better",    color: "#2e2e2e", Icon: ArrowIcon,  bg: "/col-2.png", bgPos: "left center" },
+  { name: "DEVELOPMENT",     desc: "Plays a bit, wants to get better",    color: "#2e2e2e", Icon: ArrowIcon,  bg: "/col-2.png", bgPos: "-120px center" },
   { name: "MATCH SQUAD",     desc: "Competitive league football",          color: "#1a1a1a", Icon: ShieldIcon, bg: null,         bgPos: "center"      },
   { name: "WHISTLE & BADGE", desc: "Coach or referee instead of playing", color: "#0a0a0a", Icon: StarIcon,   bg: null,         bgPos: "center"      },
 ];
